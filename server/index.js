9543635d6c3d7d942494953b3968e48a3ce8ca25
Vlad Diachenko <vdiachenko@oggettoweb.com>
2018-03-04 22:26:53 +0300

    :hammer: Fix deployment configuration

--- server/index.js
+++ server/index.js
@@ -1,9 +1,29 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const PORT = process.env.PORT || 9000
+const serve = require('koa-static')
+const Koa = require('koa')
+const config = require('config')
+const logger = require('koa-logger')
+const err = require('./middleware/error')
+const { routes, allowedMethods } = require('./middleware/routes')
 
-server.use(router)
-server.listen(PORT, () => {
-    console.log('JSON Server is running on port:', PORT)
+const app = new Koa()
+const port = process.env.PORT || 9000
+
+app.use(err)
+app.use(serve('.'))
+app.use(logger())
+app.use(routes())
+app.use(allowedMethods())
+
+app.listen(port, () => {
+    console.log('%s listening at port %d', config.app.name, port)
 })
+
+// const jsonServer = require('json-server')
+// const server = jsonServer.create()
+// const router = jsonServer.router('db.json')
+// const PORT = process.env.PORT || 9000
+
+// server.use(router)
+// server.listen(PORT, () => {
+//     console.log('JSON Server is running on port:', PORT)
+// })
