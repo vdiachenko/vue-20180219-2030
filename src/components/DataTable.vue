67bb69fb9ec0d0c468d0952375d7e50c7acfbd4d
Vlad Diachenko <vdiachenko@oggettoweb.com>
2018-03-04 17:37:52 +0300

    :sparkles: Fetch and render users list

---
+++ src/components/DataTable.vue
@@ -0,0 +1,46 @@
+<template>
+    <table class="table">
+        <thead>
+            <slot name="head"></slot>
+        </thead>
+  
+        <tbody>
+            <tr v-for="item in list" :key="item.id">
+                <slot name="row" v-bind="item"></slot>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    data: () => ({
+        list: []
+    }),
+
+    props: {
+        url: {
+            type: String,
+            required: true
+        }
+    },
+
+    mounted() {
+        this.fetchData()
+    },
+
+    methods: {
+        fetchData() {
+            axios.get(this.url).then(({ data }) => {
+                this.list = data
+            })
+        }
+    }
+}
+</script>
+
+<style lang="" scoped>
+
+</style>
--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -1,13 +1,75 @@
 <template>
     <section class="section">
         <div class="container">
-            <h1 class="title">Users list page</h1>
+            <h1 class="title">Users List</h1>
+
+            <data-table :url="url">
+                <tr slot="head">
+                    <th><abbr title="Position">#</abbr></th>
+                    <th><abbr title="First Name">First Name</abbr></th>
+                    <th><abbr title="Last Name">Last Name</abbr></th>
+                    <th><abbr title="Age">Age</abbr></th>
+                    <th><abbr title="Active">Active</abbr></th>
+                    <th><abbr title="Balance">Balance</abbr></th>
+                    <th><abbr title="E-mail">E-mail</abbr></th>
+                    <th><abbr title="Phone">Phone</abbr></th>
+                    <th><abbr title="Registered">Registered</abbr></th>
+                </tr>
+
+                <template slot="row" slot-scope="props">
+                    <td>
+                        {{ props.id }}
+                    </td>
+
+                    <td>
+                        {{ props.firstName }}
+                    </td>
+
+                    <td>
+                        {{ props.lastName }}
+                    </td>
+
+                    <td>
+                        {{ props.age }}
+                    </td>
+
+                    <td>
+                        {{ props.isActive }}
+                    </td>
+
+                    <td>
+                        {{ props.balance }}
+                    </td>
+
+                    <td>
+                        {{ props.email }}
+                    </td>
+
+                    <td>
+                        {{ props.phone }}
+                    </td>
+
+                    <td>
+                        {{ props.registered }}
+                    </td>
+                </template>
+            </data-table>
         </div>
     </section>
 </template>
 
 <script>
-export default {}
+import DataTable from '@/components/DataTable.vue'
+
+export default {
+    data() {
+        return {
+            url: 'http://localhost:9000/users/'
+        }
+    },
+
+    components: { DataTable }
+}
 </script>
 
 <style lang="less" scoped>
